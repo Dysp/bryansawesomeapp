9570ae5ff71e004ca3377b112015d32ac0ce92dd
Bryan <cortinabryan@gmail.com>
2021-06-27 10:17:31 -0700

    transfered the rest of the recovery files

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   root 'pages#login'
   get 'index', to: 'pages#index'
+  get 'ranks', to: 'pages#ranks'
+  get 'contact', to: 'pages#contact'
+  get 'billing', to: 'pages#billing'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
